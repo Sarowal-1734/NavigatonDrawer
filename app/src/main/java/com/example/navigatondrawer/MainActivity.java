4793f37769a86faa4168b9e7629b7f4b1f296dab
sarowalhossain <sarowalhossain1997@gmail.com>
2021-04-16 15:04:34 +0600

    Initial Commit...

---
+++ app/src/main/java/com/example/navigatondrawer/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.navigatondrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewProfile;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        fab = findViewById(R.id.fab);
+
+        imageViewProfile = findViewById(R.id.imageViewProfile);
+        imageViewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(Gravity.RIGHT);
+                fab.hide();
+            }
+        });
+
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+                fab.hide();
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                fab.show();
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+    }
+}
